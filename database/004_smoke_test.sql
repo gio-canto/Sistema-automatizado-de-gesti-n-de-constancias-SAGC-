55af3368114baba387d9b9ea5739e4b5dab896db
Gio Antonio Canto Gómez <gioantonio98@gmail.com>
2026-09-08 11:14:13 -0600

    Expand database smoke test for canonical schema

--- database/004_smoke_test.sql
+++ database/004_smoke_test.sql
@@ -7,6 +7,14 @@ SHOW TABLES;
 SELECT * FROM tipos_documento ORDER BY id_tipo_documento;
 
 SELECT
+  serie,
+  anio,
+  generacion,
+  ultimo_valor
+FROM contador_folios
+ORDER BY anio DESC, generacion, serie;
+
+SELECT
   TABLE_NAME,
   TABLE_ROWS,
   ENGINE,
@@ -14,3 +22,11 @@ SELECT
 FROM information_schema.TABLES
 WHERE TABLE_SCHEMA = 'sagc'
 ORDER BY TABLE_NAME;
+
+SELECT
+  CONSTRAINT_NAME,
+  TABLE_NAME,
+  REFERENCED_TABLE_NAME
+FROM information_schema.REFERENTIAL_CONSTRAINTS
+WHERE CONSTRAINT_SCHEMA = 'sagc'
+ORDER BY TABLE_NAME, CONSTRAINT_NAME;
