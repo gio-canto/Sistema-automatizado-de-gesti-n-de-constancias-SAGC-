4a318e4edbd31c6fbd9bd0c52c46e6b1889abc5b
Gio Antonio Canto Gómez <gioantonio98@gmail.com>
2026-09-10 12:03:43 -0600

    Add frontend branding assets module

---
+++ src/assets/branding.js
@@ -0,0 +1,9 @@
+export const OFFICIAL_LOGO =
+  'https://www.fesgro.cocytieg.gob.mx/images/logo_horizontal_w_256.png';
+
+export const LOGIN_IMAGES = Object.freeze([
+  'https://firebasestorage.googleapis.com/v0/b/fesgro-cocytieg.appspot.com/o/src%2FloginImage%2Fimagen1.jpg?alt=media',
+  'https://firebasestorage.googleapis.com/v0/b/fesgro-cocytieg.appspot.com/o/src%2FloginImage%2Fimagen2.jpg?alt=media',
+  'https://firebasestorage.googleapis.com/v0/b/fesgro-cocytieg.appspot.com/o/src%2FloginImage%2Fimagen3.jpg?alt=media',
+  'https://firebasestorage.googleapis.com/v0/b/fesgro-cocytieg.appspot.com/o/src%2FloginImage%2Fimagen4.jpg?alt=media',
+]);
