e237bb5dc7333d87b80e7e01e9878f3370bc1db5
Gio Antonio Canto Gómez <gioantonio98@gmail.com>
2026-09-17 11:44:50 -0600

    Load role workspace styles

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import 'sileo/styles.css';
 import App from './App.jsx';
 import './styles/main.css';
 import './styles/ui-system.css';
+import './styles/workspace.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
