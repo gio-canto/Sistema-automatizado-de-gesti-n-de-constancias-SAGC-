1ff7e9af6716e026b02a3a9c377f018e038a052b
Gio Antonio Canto Gómez <gioantonio98@gmail.com>
2026-09-18 12:23:13 -0600

    Remove obsolete CAP file: server/src/scripts/generate-cap-secret.js

--- server/src/scripts/generate-cap-secret.js
+++
@@ -1,7 +0,0 @@
-import { randomBytes } from 'node:crypto';
-
-const secret = randomBytes(32).toString('base64url');
-
-console.log('CAP_SECRET=' + secret);
-console.log('');
-console.log('Copie esta línea a server/.env. No la publique ni la coloque en Vite.');
